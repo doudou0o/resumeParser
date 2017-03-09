bcbd92519ebd7ab8822ff263d9881caa00644977
doudou0o <bluecc021@gmail.com>
2017-03-09 15:13:44 +0800

    add FileUtil and StringUtil

---
+++ src/main/java/com/echeng/resumeparser/common/utils/FileUtil.java
@@ -0,0 +1,7 @@
+package com.echeng.resumeparser.common.utils;
+
+import org.apache.commons.io.FileUtils;
+
+public class FileUtil extends FileUtils {
+
+}
--- src/main/java/com/echeng/resumeparser/common/utils/StringUtil.java
+++ src/main/java/com/echeng/resumeparser/common/utils/StringUtil.java
@@ -1,5 +1,35 @@
 package com.echeng.resumeparser.common.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringUtil {
+
+	public static List<String[]> travelListWithThreeLines(List<String> lines){
+		List<String[]> threeLines = new ArrayList<String[]>(lines.size());
+		for (int i = 0; i < lines.size(); i++) {
+			String[] t = new String[3];
+			t[1] = i-1<0 ? "" : lines.get(i-1);
+			t[2] = lines.get(i);
+			t[3] = i+1>=lines.size() ? "" : lines.get(i+1);
+			threeLines.add(t);
+		}
+		return threeLines;
+	}
+
+	public static List<String[]> travelListWithFiveLines(List<String> lines){
+		List<String[]> fiveLines = new ArrayList<String[]>(lines.size());
+		for (int i = 0; i < lines.size(); i++) {
+			String[] t = new String[5];
+			t[0] = i-2<0 ? "" : lines.get(i-2);
+			t[1] = i-1<0 ? "" : lines.get(i-1);
+			t[2] = lines.get(i);
+			t[3] = i+1>=lines.size() ? "" : lines.get(i+1);
+			t[4] = i+2>=lines.size() ? "" : lines.get(i+2);
+			fiveLines.add(t);
+		}
+		return fiveLines;
+	}
 
 }
